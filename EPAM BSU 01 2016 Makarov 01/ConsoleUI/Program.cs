1c7e12c90ab0dc35a61f0ad4c39dc81293c9c3b1
RomanMakarov1002 <roman.makarov1002@gmail.com>
2016-03-08 19:20:59 +0300

    Newton method for finding roots of  num

---
+++ EPAM BSU 01 2016 Makarov 01/ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NewtonMethod.Logic.SqrtN(125, -3, 00.1));
+            Console.WriteLine(Math.Pow(125 , (double) -1/3));
+            Console.WriteLine(NewtonMethod.Logic.SqrtN(0, 100, 00.1));
+            Console.WriteLine(Math.Pow(0, (double)1 / 100));
+            Console.WriteLine(NewtonMethod.Logic.SqrtN(-125, 3, 00.1));
+            Console.WriteLine(NewtonMethod.Logic.SqrtN(100, 5, 0.0001));
+            Console.WriteLine(Math.Pow(100, (double) 1/5));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EPAM BSU 01 2016 Makarov 01/NewtonMethod/Logic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewtonMethod
+{
+    public class Logic
+    {
+        /// <summary>
+        /// Returns SqrtN
+        /// </summary>
+        /// <param name="A">The Number whose root is to be found</param>
+        /// <param name="n">power</param>
+        /// <param name="eps">accuracy</param>
+        public static double SqrtN (double a, double n, double eps)
+        {
+            if (a == 0)
+                return 0;
+            if ((a < 0 && n % 2 == 0) || (n==0))
+                throw new ArgumentException();
+            if (n < 0)
+                return 1 / NewtonMethod(a, Math.Abs(n), eps);
+            return NewtonMethod(a, n, eps);
+        }
+
+        private static double NewtonMethod(double A, double n, double eps)
+        {
+            double x0 = A / n;
+            double x1 = (1 / n) * ((n - 1) * x0 + A / Math.Pow(x0, n - 1));
+            while (Math.Abs(x1 - x0) > eps)
+            {
+                x0 = x1;
+                x1 = (1 / n) * ((n - 1) * x0 + A / Math.Pow(x0, n - 1));
+            }
+            return x1;
+        }
+    }
+}
