814f282f2b03c128eb456a5798abc8264833d133
RomanMakarov1002 <roman.makarov1002@gmail.com>
2016-03-13 20:22:30 +0300

    "Adding Unit tests"

--- EPAM BSU 01 2016 Makarov 01/NewtonMethod/Logic.cs
+++ EPAM BSU 01 2016 Makarov 01/NewtonMethod/Logic.cs
@@ -11,28 +11,28 @@ namespace NewtonMethod
         /// <summary>
         /// Returns SqrtN
         /// </summary>
-        /// <param name="A">The Number whose root is to be found</param>
+        /// <param name="num">The Number whose root is to be found</param>
         /// <param name="n">power</param>
         /// <param name="eps">accuracy</param>
-        public static double SqrtN (double a, double n, double eps)
+        public static double SqrtN (double num, int n, double eps)
         {
-            if (a == 0)
+            if (num == 0)
                 return 0;
-            if ((a < 0 && n % 2 == 0) || (n==0))
+            if ((num < 0 && n % 2 == 0) || (n==0))
                 throw new ArgumentException();
             if (n < 0)
-                return 1 / NewtonMethod(a, Math.Abs(n), eps);
-            return NewtonMethod(a, n, eps);
+                return 1 / NewtonMethod(num, Math.Abs(n), eps);
+            return NewtonMethod(num, n, eps);
         }
 
-        private static double NewtonMethod(double A, double n, double eps)
+        private static double NewtonMethod(double num, int n, double eps)
         {
-            double x0 = A / n;
-            double x1 = (1 / n) * ((n - 1) * x0 + A / Math.Pow(x0, n - 1));
+            double x0 = num / n;
+            double x1 = (1.0 / n) * ((n - 1) * x0 + num / Math.Pow(x0, n - 1));
             while (Math.Abs(x1 - x0) > eps)
             {
                 x0 = x1;
-                x1 = (1 / n) * ((n - 1) * x0 + A / Math.Pow(x0, n - 1));
+                x1 = (1.0 / n) * ((n - 1) * x0 + num / Math.Pow(x0, n - 1));
             }
             return x1;
         }
---
+++ EPAM BSU 01 2016 Makarov 01/Tests/NewtonMethodTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using static NewtonMethod.Logic;
+
+namespace Tests
+{
+    [TestFixture]
+    public class NewtonMethodTests
+    {
+        [TestCase(0, 5, 00.1, ExpectedResult = 0)]
+        [TestCase(-25, 2 ,0.0001, ExpectedException = typeof(ArgumentException))]
+        public double SqrtN_Test(double num, int root, double e)
+        {
+            return SqrtN(num, root, e) ;
+        }
+        [Test]
+        public void SqrtN_CompareWithMathPowResults()
+        {
+            double[] nums = new double[5] {125, -125, 0, 100, -100};
+            int[] roots = new int[5] {3, 3, 1, 5, 5};
+            double eps = 0.001;
+            double[] results = new double[5] {5, -5, 0, 2.5118, -2.5118};
+            for (int i=0; i< nums.Length; i++)
+                Assert.That(SqrtN(nums[i], roots[i], eps), Is.InRange(results[i] - eps, results[i] + eps));
+        }
+
+    }
+}
